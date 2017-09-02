2fd509a652032f0a64a74bb33474a260fe5b8cd6
yuehchuan <schwarmcyc@hotmail.com>
2017-09-03 02:49:04 +0800

    control subscriber with thread

--- catkin_ws/src/rmp_base/src/rmp_base.py
+++ catkin_ws/src/rmp_base/src/rmp_base.py
@@ -11,7 +11,7 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import Imu,JointState
 from rmp_msgs.msg import BoolStamped,AudioCommand,FaultStatus
-
+import threading
 
 
 """
@@ -55,9 +55,12 @@ class rmp_base(object):
 
         rospy.loginfo("[%s] Initialzed." %(self.node_name))
 
-        #self.thread_lock = threading.Lock()
-        self.active = False
-        #self.rmp_thread()//start everything
+        #thread lock for control traffic flow for subscriber
+        self.thread_lock = threading.Lock()
+        self.active = True
+
+
+
     def setupParameter(self,param_name,default_value):
         value = rospy.get_param(param_name,default_value)
         rospy.set_param(param_name,value) #Write to parameter server for transparancy
@@ -66,17 +69,35 @@ class rmp_base(object):
 
     def cbTopic(self,msg):
         #rospy.loginfo("[%s] %s" %(self.node_name,msg.data))
-        #skip "if not" part if self.active == true
+        #skip "if not" session if "self.active == true"
         if not self.active:
                     rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z))
                     rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z))
                     rospy.loginfo("From Houston!")
-        return
+                    return
 
-	#	thread = threading.Thread(target=self.rmp_thread,args=(msg,))
-	#	thread.setDaemon(True)
-    #    thread.start()
+        thread = threading.Thread(target=self.set_velocity,args=(msg,))
+        thread.setDaemon(True)
+        thread.start()
 
+
+
+    def set_linear_velocity(self,msg):
+        rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z))
+        self.thread_lock.release()
+    def set_angular_velocity(self,msg):
+        rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z))
+        self.thread_lock.release()
+
+    def set_velocity(self,msg):
+
+        self.thread_lock.acquire()
+        if abs(msg.twist.linear.x)>0.0:
+            self.set_linear_velocity(msg)
+        elif abs(msg.twist.angular.z)>0.0:
+            self.set_angular_velocity(msg)
+        else:
+            self.thread_lock.release()
 
     def cbTimer(self,event):
         singer = HelloGoodbye()
@@ -88,96 +109,37 @@ class rmp_base(object):
     def on_shutdown(self):
         rospy.loginfo("[%s] Shutting down." %(self.node_name))
 
-    def rmp_thread(self):
-
-        """
-        Create and response and command queue. The responses will be in the form of
-        a dictionary containing the vaiable name as the key and a converted value
-        the names are defined in the feedback_X_bitmap_menu_items dictionaries if a particular
-        variable is of interest
-        """
-        rsp_queue = Queue.Queue()
-        cmd_queue = Queue.Queue()
-        in_flags  = Queue.Queue()
-        out_flags = Queue.Queue()
-
-        """
-        Create the thread to run RMP
-        """
-        my_thread = threading.Thread(target=RMP, args=(rmp_addr,rsp_queue,cmd_queue,in_flags,out_flags,UPDATE_DELAY_SEC,LOG_DATA))
-        my_thread.daemon = True
-        my_thread.start()
-
-        """
-        Initialize my event handler class
-        """
-        EventHandler = RMPEventHandlers(cmd_queue,rsp_queue,in_flags)
-
-        """
-        -------------------------------------------------------------------------------
-        User loop starts here modify to make it do what you want.
-
-        You can pipe std_in from another application to the command queue and the response to std out or
-        let the event handlers define everything. That is up to the user. In this example we transition modes,
-        send motion commands (zeroed), play audio songs, and print the response dictionary. The application
-        terminates the thread and exits when all the songs have been played. It is just an example of how to
-        spawn a RMP thread, handle events, and send/receive data
-        -------------------------------------------------------------------------------
-        """
-
-        """
-        Generate a goto tractor event
-        """
-        #EventHandler.GotoTractor()
-        #EventHandler.GotoStandby()
-        EventHandler.GotoBalance()
-
-        """
-        Run until signaled to stop
-        Perform the actions defined based on the flags passed out
-        """
-        while (True == EventHandler._continue):
-            EventHandler.handle_event[RMP_ROTATE]()
-            EventHandler.handle_event[RMP_RSP_DATA_RDY]()
-            time.sleep(1.0)
-            EventHandler.handle_event[RMP_ZERO]()
-            EventHandler.handle_event[RMP_RSP_DATA_RDY]()
-            time.sleep(1.0)
-            EventHandler.handle_event[RMP_FORWARD]()
-            EventHandler.handle_event[RMP_RSP_DATA_RDY]()
-            time.sleep(1.0)
-            pass
-            #while not out_flags.empty():
-            #  EventHandler.handle_event[out_flags.get()]()
-        """
-        Generate a goto standby event
-        """
-
-        """
-        Allow enough time for the command to be sent
-        then send the signal to kill the thread
-        """
-        time.sleep(1.0)
-        in_flags.put(RMP_KILL)
-
-        """
-        Wait for the thread to die
-        """
-        while my_thread.isAlive():
-            pass
-
-        """
-        Exit main
-        """
-        sys.exit()
 if __name__ == '__main__':
     # Initialize the node with rospy
     rospy.init_node('rmp_base', anonymous=False)
+    #setup communication thread
+    rsp_queue = Queue.Queue()
+    cmd_queue = Queue.Queue()
+    in_flags  = Queue.Queue()
+    out_flags = Queue.Queue()
+#    my_thread = threading.Thread(target=RMP, args=(rmp_addr,rsp_queue,cmd_queue,in_flags,out_flags,UPDATE_DELAY_SEC,LOG_DATA))
+#    my_thread.setDaemon(True)
+#    my_thread.start()
+
+    #svreate event handler and set to BALANCE MODE
+    EventHandler = RMPEventHandlers(cmd_queue,rsp_queue,in_flags)
+
+    #go to BALANCE
+    EventHandler.GotoBalance()
+    rospy.loginfo("finish initialize!")
 
     # Create the NodeName object
     node = rmp_base()
+
+#    while my_thread.isAlive():
+#        pass
+
+
+
+
     # Setup proper shutdown behavior
     rospy.on_shutdown(node.on_shutdown)
 
     # Keep it spinning to keep the node alive
     rospy.spin()
+    sys.exit()
